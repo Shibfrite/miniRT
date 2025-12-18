26a6994eb9dada51694ee5767a29315001118579
Yyunozor <yyunozor@icloud.com>
2025-12-19 00:33:48 +0100

    small fix

--- includes/parser.h
+++ includes/parser.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/18 11:00:00 by anpayot           #+#    #+#             */
-/*   Updated: 2025/12/18 10:53:22 by anpayot          ###   ########.fr       */
+/*   Created: 2025/12/18 10:00:00 by anpayot           #+#    #+#             */
+/*   Updated: 2025/12/19 00:30:42 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,15 @@
 # define ERR_OPEN		"Error: Cannot open file"
 
 //-------------Element identifiers in .rt files-------------
-// A  0.2              255,255,255       (ambient)
-// C  -50,0,20         0,0,1       70    (camera)
-// L  -40,50,0         0.6               (light)
-// sp 0,0,20           20          255,0,0     (sphere)
-// pl 0,0,-10          0,1,0       0,0,225     (plane)
-// cy 50,0,20.6        0,0,1       14.2  21.42  10,0,255  (cylinder)
+// ID	Position		Direction		Param1		Param2		Color		(Type)
+
+// A	0.2				255,255,255		-			-			-			(ambient)
+// C	-50,0,20		0,0,1			70			-			-			(camera)
+// L	-40,50,0		-				0.6			-			-			(light)
+// sp	0,0,20			-				20			-			255,0,0		(sphere)
+// pl	0,0,-10			0,1,0			-			-			0,0,225		(plane)
+// cy	50,0,20.6		0,0,1			14.2		21.42		10,0,255	(cylinder)
+
 
 //-------------Parsing functions-------------
 int		parse_scene(char *filename);
